37c684e1d9fa176b729eec8b9cb4824ac37d8c64
Ziggy <ziggy.attala@york.ac.uk>
2024-05-26 20:18:35 +0100

    finished collator completely, m2t generates channels but not the process right now

---
+++ epsilon_transformation/circus_gen/circus_gen/expr.thy
@@ -0,0 +1,21 @@
+theory AnglePIDANN_CirCus
+	imports Axiomatic_Circus
+begin
+
+chantype chan = 
+	nodeOut211 :: Value	
+	layerRes21 :: Value	
+	nodeOut112 :: Value	
+	nodeOut111 :: Value	
+	layerRes11 :: Value	
+	layerRes02 :: Value	
+	layerRes01 :: Value	
+	terminate :: unit 	
+  
+ 
+
+end
+
+
+
+
