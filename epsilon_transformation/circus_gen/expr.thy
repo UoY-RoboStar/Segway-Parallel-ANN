34a17f56dc98a4a6d5252df8f1fb024da07b4481
Ziggy <ziggy.attala@york.ac.uk>
2024-05-31 12:52:44 +0100

    added m2t validation, and made minor fixes to ann_generation, using real for now instead of Value

--- epsilon_transformation/circus_gen/expr.thy
+++ epsilon_transformation/circus_gen/expr.thy
@@ -5,22 +5,10 @@ begin
 
 	
 chantype  = 
-	nodeOut211 :: Value	
-	
-chantype  = 
-	layerRes21 :: Value	
-	
-chantype  = 
-	nodeOut112 :: Value	
-	
-chantype  = 
 	nodeOut111 :: Value	
 	
 chantype  = 
 	layerRes11 :: Value	
-	
-chantype  = 
-	layerRes02 :: Value	
 	
 chantype  = 
 	layerRes01 :: Value	
