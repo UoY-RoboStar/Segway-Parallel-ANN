a921d1b54bf84a7e9f48ccf18abeb9c1101d8e84
Ziggy <ziggy.attala@york.ac.uk>
2024-06-01 11:31:05 +0100

    added first succesful test of just collator

---
+++ epsilon_transformation/isabelle_out/isabelle_output_123.thy
@@ -0,0 +1,771 @@
+theory test64_CirCus
+	imports Axiomatic_Circus
+begin
+
+
+
+	
+chantype nodeOut31010 = 
+	nodeOut31010 :: real	
+	
+chantype nodeOut3109 = 
+	nodeOut3109 :: real	
+	
+chantype nodeOut3108 = 
+	nodeOut3108 :: real	
+	
+chantype nodeOut3107 = 
+	nodeOut3107 :: real	
+	
+chantype nodeOut3106 = 
+	nodeOut3106 :: real	
+	
+chantype nodeOut3105 = 
+	nodeOut3105 :: real	
+	
+chantype nodeOut3104 = 
+	nodeOut3104 :: real	
+	
+chantype nodeOut3103 = 
+	nodeOut3103 :: real	
+	
+chantype nodeOut3102 = 
+	nodeOut3102 :: real	
+	
+chantype nodeOut3101 = 
+	nodeOut3101 :: real	
+	
+chantype layerRes310 = 
+	layerRes310 :: real	
+	
+chantype nodeOut3910 = 
+	nodeOut3910 :: real	
+	
+chantype nodeOut399 = 
+	nodeOut399 :: real	
+	
+chantype nodeOut398 = 
+	nodeOut398 :: real	
+	
+chantype nodeOut397 = 
+	nodeOut397 :: real	
+	
+chantype nodeOut396 = 
+	nodeOut396 :: real	
+	
+chantype nodeOut395 = 
+	nodeOut395 :: real	
+	
+chantype nodeOut394 = 
+	nodeOut394 :: real	
+	
+chantype nodeOut393 = 
+	nodeOut393 :: real	
+	
+chantype nodeOut392 = 
+	nodeOut392 :: real	
+	
+chantype nodeOut391 = 
+	nodeOut391 :: real	
+	
+chantype layerRes39 = 
+	layerRes39 :: real	
+	
+chantype nodeOut3810 = 
+	nodeOut3810 :: real	
+	
+chantype nodeOut389 = 
+	nodeOut389 :: real	
+	
+chantype nodeOut388 = 
+	nodeOut388 :: real	
+	
+chantype nodeOut387 = 
+	nodeOut387 :: real	
+	
+chantype nodeOut386 = 
+	nodeOut386 :: real	
+	
+chantype nodeOut385 = 
+	nodeOut385 :: real	
+	
+chantype nodeOut384 = 
+	nodeOut384 :: real	
+	
+chantype nodeOut383 = 
+	nodeOut383 :: real	
+	
+chantype nodeOut382 = 
+	nodeOut382 :: real	
+	
+chantype nodeOut381 = 
+	nodeOut381 :: real	
+	
+chantype layerRes38 = 
+	layerRes38 :: real	
+	
+chantype nodeOut3710 = 
+	nodeOut3710 :: real	
+	
+chantype nodeOut379 = 
+	nodeOut379 :: real	
+	
+chantype nodeOut378 = 
+	nodeOut378 :: real	
+	
+chantype nodeOut377 = 
+	nodeOut377 :: real	
+	
+chantype nodeOut376 = 
+	nodeOut376 :: real	
+	
+chantype nodeOut375 = 
+	nodeOut375 :: real	
+	
+chantype nodeOut374 = 
+	nodeOut374 :: real	
+	
+chantype nodeOut373 = 
+	nodeOut373 :: real	
+	
+chantype nodeOut372 = 
+	nodeOut372 :: real	
+	
+chantype nodeOut371 = 
+	nodeOut371 :: real	
+	
+chantype layerRes37 = 
+	layerRes37 :: real	
+	
+chantype nodeOut3610 = 
+	nodeOut3610 :: real	
+	
+chantype nodeOut369 = 
+	nodeOut369 :: real	
+	
+chantype nodeOut368 = 
+	nodeOut368 :: real	
+	
+chantype nodeOut367 = 
+	nodeOut367 :: real	
+	
+chantype nodeOut366 = 
+	nodeOut366 :: real	
+	
+chantype nodeOut365 = 
+	nodeOut365 :: real	
+	
+chantype nodeOut364 = 
+	nodeOut364 :: real	
+	
+chantype nodeOut363 = 
+	nodeOut363 :: real	
+	
+chantype nodeOut362 = 
+	nodeOut362 :: real	
+	
+chantype nodeOut361 = 
+	nodeOut361 :: real	
+	
+chantype layerRes36 = 
+	layerRes36 :: real	
+	
+chantype nodeOut3510 = 
+	nodeOut3510 :: real	
+	
+chantype nodeOut359 = 
+	nodeOut359 :: real	
+	
+chantype nodeOut358 = 
+	nodeOut358 :: real	
+	
+chantype nodeOut357 = 
+	nodeOut357 :: real	
+	
+chantype nodeOut356 = 
+	nodeOut356 :: real	
+	
+chantype nodeOut355 = 
+	nodeOut355 :: real	
+	
+chantype nodeOut354 = 
+	nodeOut354 :: real	
+	
+chantype nodeOut353 = 
+	nodeOut353 :: real	
+	
+chantype nodeOut352 = 
+	nodeOut352 :: real	
+	
+chantype nodeOut351 = 
+	nodeOut351 :: real	
+	
+chantype layerRes35 = 
+	layerRes35 :: real	
+	
+chantype nodeOut3410 = 
+	nodeOut3410 :: real	
+	
+chantype nodeOut349 = 
+	nodeOut349 :: real	
+	
+chantype nodeOut348 = 
+	nodeOut348 :: real	
+	
+chantype nodeOut347 = 
+	nodeOut347 :: real	
+	
+chantype nodeOut346 = 
+	nodeOut346 :: real	
+	
+chantype nodeOut345 = 
+	nodeOut345 :: real	
+	
+chantype nodeOut344 = 
+	nodeOut344 :: real	
+	
+chantype nodeOut343 = 
+	nodeOut343 :: real	
+	
+chantype nodeOut342 = 
+	nodeOut342 :: real	
+	
+chantype nodeOut341 = 
+	nodeOut341 :: real	
+	
+chantype layerRes34 = 
+	layerRes34 :: real	
+	
+chantype nodeOut3310 = 
+	nodeOut3310 :: real	
+	
+chantype nodeOut339 = 
+	nodeOut339 :: real	
+	
+chantype nodeOut338 = 
+	nodeOut338 :: real	
+	
+chantype nodeOut337 = 
+	nodeOut337 :: real	
+	
+chantype nodeOut336 = 
+	nodeOut336 :: real	
+	
+chantype nodeOut335 = 
+	nodeOut335 :: real	
+	
+chantype nodeOut334 = 
+	nodeOut334 :: real	
+	
+chantype nodeOut333 = 
+	nodeOut333 :: real	
+	
+chantype nodeOut332 = 
+	nodeOut332 :: real	
+	
+chantype nodeOut331 = 
+	nodeOut331 :: real	
+	
+chantype layerRes33 = 
+	layerRes33 :: real	
+	
+chantype nodeOut3210 = 
+	nodeOut3210 :: real	
+	
+chantype nodeOut329 = 
+	nodeOut329 :: real	
+	
+chantype nodeOut328 = 
+	nodeOut328 :: real	
+	
+chantype nodeOut327 = 
+	nodeOut327 :: real	
+	
+chantype nodeOut326 = 
+	nodeOut326 :: real	
+	
+chantype nodeOut325 = 
+	nodeOut325 :: real	
+	
+chantype nodeOut324 = 
+	nodeOut324 :: real	
+	
+chantype nodeOut323 = 
+	nodeOut323 :: real	
+	
+chantype nodeOut322 = 
+	nodeOut322 :: real	
+	
+chantype nodeOut321 = 
+	nodeOut321 :: real	
+	
+chantype layerRes32 = 
+	layerRes32 :: real	
+	
+chantype nodeOut3110 = 
+	nodeOut3110 :: real	
+	
+chantype nodeOut319 = 
+	nodeOut319 :: real	
+	
+chantype nodeOut318 = 
+	nodeOut318 :: real	
+	
+chantype nodeOut317 = 
+	nodeOut317 :: real	
+	
+chantype nodeOut316 = 
+	nodeOut316 :: real	
+	
+chantype nodeOut315 = 
+	nodeOut315 :: real	
+	
+chantype nodeOut314 = 
+	nodeOut314 :: real	
+	
+chantype nodeOut313 = 
+	nodeOut313 :: real	
+	
+chantype nodeOut312 = 
+	nodeOut312 :: real	
+	
+chantype nodeOut311 = 
+	nodeOut311 :: real	
+	
+chantype layerRes31 = 
+	layerRes31 :: real	
+	
+chantype nodeOut21010 = 
+	nodeOut21010 :: real	
+	
+chantype nodeOut2109 = 
+	nodeOut2109 :: real	
+	
+chantype nodeOut2108 = 
+	nodeOut2108 :: real	
+	
+chantype nodeOut2107 = 
+	nodeOut2107 :: real	
+	
+chantype nodeOut2106 = 
+	nodeOut2106 :: real	
+	
+chantype nodeOut2105 = 
+	nodeOut2105 :: real	
+	
+chantype nodeOut2104 = 
+	nodeOut2104 :: real	
+	
+chantype nodeOut2103 = 
+	nodeOut2103 :: real	
+	
+chantype nodeOut2102 = 
+	nodeOut2102 :: real	
+	
+chantype nodeOut2101 = 
+	nodeOut2101 :: real	
+	
+chantype layerRes210 = 
+	layerRes210 :: real	
+	
+chantype nodeOut2910 = 
+	nodeOut2910 :: real	
+	
+chantype nodeOut299 = 
+	nodeOut299 :: real	
+	
+chantype nodeOut298 = 
+	nodeOut298 :: real	
+	
+chantype nodeOut297 = 
+	nodeOut297 :: real	
+	
+chantype nodeOut296 = 
+	nodeOut296 :: real	
+	
+chantype nodeOut295 = 
+	nodeOut295 :: real	
+	
+chantype nodeOut294 = 
+	nodeOut294 :: real	
+	
+chantype nodeOut293 = 
+	nodeOut293 :: real	
+	
+chantype nodeOut292 = 
+	nodeOut292 :: real	
+	
+chantype nodeOut291 = 
+	nodeOut291 :: real	
+	
+chantype layerRes29 = 
+	layerRes29 :: real	
+	
+chantype nodeOut2810 = 
+	nodeOut2810 :: real	
+	
+chantype nodeOut289 = 
+	nodeOut289 :: real	
+	
+chantype nodeOut288 = 
+	nodeOut288 :: real	
+	
+chantype nodeOut287 = 
+	nodeOut287 :: real	
+	
+chantype nodeOut286 = 
+	nodeOut286 :: real	
+	
+chantype nodeOut285 = 
+	nodeOut285 :: real	
+	
+chantype nodeOut284 = 
+	nodeOut284 :: real	
+	
+chantype nodeOut283 = 
+	nodeOut283 :: real	
+	
+chantype nodeOut282 = 
+	nodeOut282 :: real	
+	
+chantype nodeOut281 = 
+	nodeOut281 :: real	
+	
+chantype layerRes28 = 
+	layerRes28 :: real	
+	
+chantype nodeOut2710 = 
+	nodeOut2710 :: real	
+	
+chantype nodeOut279 = 
+	nodeOut279 :: real	
+	
+chantype nodeOut278 = 
+	nodeOut278 :: real	
+	
+chantype nodeOut277 = 
+	nodeOut277 :: real	
+	
+chantype nodeOut276 = 
+	nodeOut276 :: real	
+	
+chantype nodeOut275 = 
+	nodeOut275 :: real	
+	
+chantype nodeOut274 = 
+	nodeOut274 :: real	
+	
+chantype nodeOut273 = 
+	nodeOut273 :: real	
+	
+chantype nodeOut272 = 
+	nodeOut272 :: real	
+	
+chantype nodeOut271 = 
+	nodeOut271 :: real	
+	
+chantype layerRes27 = 
+	layerRes27 :: real	
+	
+chantype nodeOut2610 = 
+	nodeOut2610 :: real	
+	
+chantype nodeOut269 = 
+	nodeOut269 :: real	
+	
+chantype nodeOut268 = 
+	nodeOut268 :: real	
+	
+chantype nodeOut267 = 
+	nodeOut267 :: real	
+	
+chantype nodeOut266 = 
+	nodeOut266 :: real	
+	
+chantype nodeOut265 = 
+	nodeOut265 :: real	
+	
+chantype nodeOut264 = 
+	nodeOut264 :: real	
+	
+chantype nodeOut263 = 
+	nodeOut263 :: real	
+	
+chantype nodeOut262 = 
+	nodeOut262 :: real	
+	
+chantype nodeOut261 = 
+	nodeOut261 :: real	
+	
+chantype layerRes26 = 
+	layerRes26 :: real	
+	
+chantype nodeOut2510 = 
+	nodeOut2510 :: real	
+	
+chantype nodeOut259 = 
+	nodeOut259 :: real	
+	
+chantype nodeOut258 = 
+	nodeOut258 :: real	
+	
+chantype nodeOut257 = 
+	nodeOut257 :: real	
+	
+chantype nodeOut256 = 
+	nodeOut256 :: real	
+	
+chantype nodeOut255 = 
+	nodeOut255 :: real	
+	
+chantype nodeOut254 = 
+	nodeOut254 :: real	
+	
+chantype nodeOut253 = 
+	nodeOut253 :: real	
+	
+chantype nodeOut252 = 
+	nodeOut252 :: real	
+	
+chantype nodeOut251 = 
+	nodeOut251 :: real	
+	
+chantype layerRes25 = 
+	layerRes25 :: real	
+	
+chantype nodeOut2410 = 
+	nodeOut2410 :: real	
+	
+chantype nodeOut249 = 
+	nodeOut249 :: real	
+	
+chantype nodeOut248 = 
+	nodeOut248 :: real	
+	
+chantype nodeOut247 = 
+	nodeOut247 :: real	
+	
+chantype nodeOut246 = 
+	nodeOut246 :: real	
+	
+chantype nodeOut245 = 
+	nodeOut245 :: real	
+	
+chantype nodeOut244 = 
+	nodeOut244 :: real	
+	
+chantype nodeOut243 = 
+	nodeOut243 :: real	
+	
+chantype nodeOut242 = 
+	nodeOut242 :: real	
+	
+chantype nodeOut241 = 
+	nodeOut241 :: real	
+	
+chantype layerRes24 = 
+	layerRes24 :: real	
+	
+chantype nodeOut2310 = 
+	nodeOut2310 :: real	
+	
+chantype nodeOut239 = 
+	nodeOut239 :: real	
+	
+chantype nodeOut238 = 
+	nodeOut238 :: real	
+	
+chantype nodeOut237 = 
+	nodeOut237 :: real	
+	
+chantype nodeOut236 = 
+	nodeOut236 :: real	
+	
+chantype nodeOut235 = 
+	nodeOut235 :: real	
+	
+chantype nodeOut234 = 
+	nodeOut234 :: real	
+	
+chantype nodeOut233 = 
+	nodeOut233 :: real	
+	
+chantype nodeOut232 = 
+	nodeOut232 :: real	
+	
+chantype nodeOut231 = 
+	nodeOut231 :: real	
+	
+chantype layerRes23 = 
+	layerRes23 :: real	
+	
+chantype nodeOut2210 = 
+	nodeOut2210 :: real	
+	
+chantype nodeOut229 = 
+	nodeOut229 :: real	
+	
+chantype nodeOut228 = 
+	nodeOut228 :: real	
+	
+chantype nodeOut227 = 
+	nodeOut227 :: real	
+	
+chantype nodeOut226 = 
+	nodeOut226 :: real	
+	
+chantype nodeOut225 = 
+	nodeOut225 :: real	
+	
+chantype nodeOut224 = 
+	nodeOut224 :: real	
+	
+chantype nodeOut223 = 
+	nodeOut223 :: real	
+	
+chantype nodeOut222 = 
+	nodeOut222 :: real	
+	
+chantype nodeOut221 = 
+	nodeOut221 :: real	
+	
+chantype layerRes22 = 
+	layerRes22 :: real	
+	
+chantype nodeOut2110 = 
+	nodeOut2110 :: real	
+	
+chantype nodeOut219 = 
+	nodeOut219 :: real	
+	
+chantype nodeOut218 = 
+	nodeOut218 :: real	
+	
+chantype nodeOut217 = 
+	nodeOut217 :: real	
+	
+chantype nodeOut216 = 
+	nodeOut216 :: real	
+	
+chantype nodeOut215 = 
+	nodeOut215 :: real	
+	
+chantype nodeOut214 = 
+	nodeOut214 :: real	
+	
+chantype nodeOut213 = 
+	nodeOut213 :: real	
+	
+chantype nodeOut212 = 
+	nodeOut212 :: real	
+	
+chantype nodeOut211 = 
+	nodeOut211 :: real	
+	
+chantype layerRes21 = 
+	layerRes21 :: real	
+	
+chantype nodeOut1102 = 
+	nodeOut1102 :: real	
+	
+chantype nodeOut1101 = 
+	nodeOut1101 :: real	
+	
+chantype layerRes110 = 
+	layerRes110 :: real	
+	
+chantype nodeOut192 = 
+	nodeOut192 :: real	
+	
+chantype nodeOut191 = 
+	nodeOut191 :: real	
+	
+chantype layerRes19 = 
+	layerRes19 :: real	
+	
+chantype nodeOut182 = 
+	nodeOut182 :: real	
+	
+chantype nodeOut181 = 
+	nodeOut181 :: real	
+	
+chantype layerRes18 = 
+	layerRes18 :: real	
+	
+chantype nodeOut172 = 
+	nodeOut172 :: real	
+	
+chantype nodeOut171 = 
+	nodeOut171 :: real	
+	
+chantype layerRes17 = 
+	layerRes17 :: real	
+	
+chantype nodeOut162 = 
+	nodeOut162 :: real	
+	
+chantype nodeOut161 = 
+	nodeOut161 :: real	
+	
+chantype layerRes16 = 
+	layerRes16 :: real	
+	
+chantype nodeOut152 = 
+	nodeOut152 :: real	
+	
+chantype nodeOut151 = 
+	nodeOut151 :: real	
+	
+chantype layerRes15 = 
+	layerRes15 :: real	
+	
+chantype nodeOut142 = 
+	nodeOut142 :: real	
+	
+chantype nodeOut141 = 
+	nodeOut141 :: real	
+	
+chantype layerRes14 = 
+	layerRes14 :: real	
+	
+chantype nodeOut132 = 
+	nodeOut132 :: real	
+	
+chantype nodeOut131 = 
+	nodeOut131 :: real	
+	
+chantype layerRes13 = 
+	layerRes13 :: real	
+	
+chantype nodeOut122 = 
+	nodeOut122 :: real	
+	
+chantype nodeOut121 = 
+	nodeOut121 :: real	
+	
+chantype layerRes12 = 
+	layerRes12 :: real	
+	
+chantype nodeOut112 = 
+	nodeOut112 :: real	
+	
+chantype nodeOut111 = 
+	nodeOut111 :: real	
+	
+chantype layerRes11 = 
+	layerRes11 :: real	
+	
+chantype layerRes02 = 
+	layerRes02 :: real	
+	
+chantype layerRes01 = 
+	layerRes01 :: real	
+	
+chantype terminate = 
+	terminate :: unit 	
+
+ 
+
+end
+
+
---
+++ epsilon_transformation/isabelle_out/isabelle_output_63.thy
@@ -0,0 +1,654 @@
+theory test155_CirCus
+	imports Axiomatic_Circus
+begin
+
+
+
+	
+chantype nodeOut3810 = 
+	nodeOut3810 :: real	
+	
+chantype nodeOut389 = 
+	nodeOut389 :: real	
+	
+chantype nodeOut388 = 
+	nodeOut388 :: real	
+	
+chantype nodeOut387 = 
+	nodeOut387 :: real	
+	
+chantype nodeOut386 = 
+	nodeOut386 :: real	
+	
+chantype nodeOut385 = 
+	nodeOut385 :: real	
+	
+chantype nodeOut384 = 
+	nodeOut384 :: real	
+	
+chantype nodeOut383 = 
+	nodeOut383 :: real	
+	
+chantype nodeOut382 = 
+	nodeOut382 :: real	
+	
+chantype nodeOut381 = 
+	nodeOut381 :: real	
+	
+chantype layerRes38 = 
+	layerRes38 :: real	
+	
+chantype nodeOut3710 = 
+	nodeOut3710 :: real	
+	
+chantype nodeOut379 = 
+	nodeOut379 :: real	
+	
+chantype nodeOut378 = 
+	nodeOut378 :: real	
+	
+chantype nodeOut377 = 
+	nodeOut377 :: real	
+	
+chantype nodeOut376 = 
+	nodeOut376 :: real	
+	
+chantype nodeOut375 = 
+	nodeOut375 :: real	
+	
+chantype nodeOut374 = 
+	nodeOut374 :: real	
+	
+chantype nodeOut373 = 
+	nodeOut373 :: real	
+	
+chantype nodeOut372 = 
+	nodeOut372 :: real	
+	
+chantype nodeOut371 = 
+	nodeOut371 :: real	
+	
+chantype layerRes37 = 
+	layerRes37 :: real	
+	
+chantype nodeOut3610 = 
+	nodeOut3610 :: real	
+	
+chantype nodeOut369 = 
+	nodeOut369 :: real	
+	
+chantype nodeOut368 = 
+	nodeOut368 :: real	
+	
+chantype nodeOut367 = 
+	nodeOut367 :: real	
+	
+chantype nodeOut366 = 
+	nodeOut366 :: real	
+	
+chantype nodeOut365 = 
+	nodeOut365 :: real	
+	
+chantype nodeOut364 = 
+	nodeOut364 :: real	
+	
+chantype nodeOut363 = 
+	nodeOut363 :: real	
+	
+chantype nodeOut362 = 
+	nodeOut362 :: real	
+	
+chantype nodeOut361 = 
+	nodeOut361 :: real	
+	
+chantype layerRes36 = 
+	layerRes36 :: real	
+	
+chantype nodeOut3510 = 
+	nodeOut3510 :: real	
+	
+chantype nodeOut359 = 
+	nodeOut359 :: real	
+	
+chantype nodeOut358 = 
+	nodeOut358 :: real	
+	
+chantype nodeOut357 = 
+	nodeOut357 :: real	
+	
+chantype nodeOut356 = 
+	nodeOut356 :: real	
+	
+chantype nodeOut355 = 
+	nodeOut355 :: real	
+	
+chantype nodeOut354 = 
+	nodeOut354 :: real	
+	
+chantype nodeOut353 = 
+	nodeOut353 :: real	
+	
+chantype nodeOut352 = 
+	nodeOut352 :: real	
+	
+chantype nodeOut351 = 
+	nodeOut351 :: real	
+	
+chantype layerRes35 = 
+	layerRes35 :: real	
+	
+chantype nodeOut3410 = 
+	nodeOut3410 :: real	
+	
+chantype nodeOut349 = 
+	nodeOut349 :: real	
+	
+chantype nodeOut348 = 
+	nodeOut348 :: real	
+	
+chantype nodeOut347 = 
+	nodeOut347 :: real	
+	
+chantype nodeOut346 = 
+	nodeOut346 :: real	
+	
+chantype nodeOut345 = 
+	nodeOut345 :: real	
+	
+chantype nodeOut344 = 
+	nodeOut344 :: real	
+	
+chantype nodeOut343 = 
+	nodeOut343 :: real	
+	
+chantype nodeOut342 = 
+	nodeOut342 :: real	
+	
+chantype nodeOut341 = 
+	nodeOut341 :: real	
+	
+chantype layerRes34 = 
+	layerRes34 :: real	
+	
+chantype nodeOut3310 = 
+	nodeOut3310 :: real	
+	
+chantype nodeOut339 = 
+	nodeOut339 :: real	
+	
+chantype nodeOut338 = 
+	nodeOut338 :: real	
+	
+chantype nodeOut337 = 
+	nodeOut337 :: real	
+	
+chantype nodeOut336 = 
+	nodeOut336 :: real	
+	
+chantype nodeOut335 = 
+	nodeOut335 :: real	
+	
+chantype nodeOut334 = 
+	nodeOut334 :: real	
+	
+chantype nodeOut333 = 
+	nodeOut333 :: real	
+	
+chantype nodeOut332 = 
+	nodeOut332 :: real	
+	
+chantype nodeOut331 = 
+	nodeOut331 :: real	
+	
+chantype layerRes33 = 
+	layerRes33 :: real	
+	
+chantype nodeOut3210 = 
+	nodeOut3210 :: real	
+	
+chantype nodeOut329 = 
+	nodeOut329 :: real	
+	
+chantype nodeOut328 = 
+	nodeOut328 :: real	
+	
+chantype nodeOut327 = 
+	nodeOut327 :: real	
+	
+chantype nodeOut326 = 
+	nodeOut326 :: real	
+	
+chantype nodeOut325 = 
+	nodeOut325 :: real	
+	
+chantype nodeOut324 = 
+	nodeOut324 :: real	
+	
+chantype nodeOut323 = 
+	nodeOut323 :: real	
+	
+chantype nodeOut322 = 
+	nodeOut322 :: real	
+	
+chantype nodeOut321 = 
+	nodeOut321 :: real	
+	
+chantype layerRes32 = 
+	layerRes32 :: real	
+	
+chantype nodeOut3110 = 
+	nodeOut3110 :: real	
+	
+chantype nodeOut319 = 
+	nodeOut319 :: real	
+	
+chantype nodeOut318 = 
+	nodeOut318 :: real	
+	
+chantype nodeOut317 = 
+	nodeOut317 :: real	
+	
+chantype nodeOut316 = 
+	nodeOut316 :: real	
+	
+chantype nodeOut315 = 
+	nodeOut315 :: real	
+	
+chantype nodeOut314 = 
+	nodeOut314 :: real	
+	
+chantype nodeOut313 = 
+	nodeOut313 :: real	
+	
+chantype nodeOut312 = 
+	nodeOut312 :: real	
+	
+chantype nodeOut311 = 
+	nodeOut311 :: real	
+	
+chantype layerRes31 = 
+	layerRes31 :: real	
+	
+chantype nodeOut2105 = 
+	nodeOut2105 :: real	
+	
+chantype nodeOut2104 = 
+	nodeOut2104 :: real	
+	
+chantype nodeOut2103 = 
+	nodeOut2103 :: real	
+	
+chantype nodeOut2102 = 
+	nodeOut2102 :: real	
+	
+chantype nodeOut2101 = 
+	nodeOut2101 :: real	
+	
+chantype layerRes210 = 
+	layerRes210 :: real	
+	
+chantype nodeOut295 = 
+	nodeOut295 :: real	
+	
+chantype nodeOut294 = 
+	nodeOut294 :: real	
+	
+chantype nodeOut293 = 
+	nodeOut293 :: real	
+	
+chantype nodeOut292 = 
+	nodeOut292 :: real	
+	
+chantype nodeOut291 = 
+	nodeOut291 :: real	
+	
+chantype layerRes29 = 
+	layerRes29 :: real	
+	
+chantype nodeOut285 = 
+	nodeOut285 :: real	
+	
+chantype nodeOut284 = 
+	nodeOut284 :: real	
+	
+chantype nodeOut283 = 
+	nodeOut283 :: real	
+	
+chantype nodeOut282 = 
+	nodeOut282 :: real	
+	
+chantype nodeOut281 = 
+	nodeOut281 :: real	
+	
+chantype layerRes28 = 
+	layerRes28 :: real	
+	
+chantype nodeOut275 = 
+	nodeOut275 :: real	
+	
+chantype nodeOut274 = 
+	nodeOut274 :: real	
+	
+chantype nodeOut273 = 
+	nodeOut273 :: real	
+	
+chantype nodeOut272 = 
+	nodeOut272 :: real	
+	
+chantype nodeOut271 = 
+	nodeOut271 :: real	
+	
+chantype layerRes27 = 
+	layerRes27 :: real	
+	
+chantype nodeOut265 = 
+	nodeOut265 :: real	
+	
+chantype nodeOut264 = 
+	nodeOut264 :: real	
+	
+chantype nodeOut263 = 
+	nodeOut263 :: real	
+	
+chantype nodeOut262 = 
+	nodeOut262 :: real	
+	
+chantype nodeOut261 = 
+	nodeOut261 :: real	
+	
+chantype layerRes26 = 
+	layerRes26 :: real	
+	
+chantype nodeOut255 = 
+	nodeOut255 :: real	
+	
+chantype nodeOut254 = 
+	nodeOut254 :: real	
+	
+chantype nodeOut253 = 
+	nodeOut253 :: real	
+	
+chantype nodeOut252 = 
+	nodeOut252 :: real	
+	
+chantype nodeOut251 = 
+	nodeOut251 :: real	
+	
+chantype layerRes25 = 
+	layerRes25 :: real	
+	
+chantype nodeOut245 = 
+	nodeOut245 :: real	
+	
+chantype nodeOut244 = 
+	nodeOut244 :: real	
+	
+chantype nodeOut243 = 
+	nodeOut243 :: real	
+	
+chantype nodeOut242 = 
+	nodeOut242 :: real	
+	
+chantype nodeOut241 = 
+	nodeOut241 :: real	
+	
+chantype layerRes24 = 
+	layerRes24 :: real	
+	
+chantype nodeOut235 = 
+	nodeOut235 :: real	
+	
+chantype nodeOut234 = 
+	nodeOut234 :: real	
+	
+chantype nodeOut233 = 
+	nodeOut233 :: real	
+	
+chantype nodeOut232 = 
+	nodeOut232 :: real	
+	
+chantype nodeOut231 = 
+	nodeOut231 :: real	
+	
+chantype layerRes23 = 
+	layerRes23 :: real	
+	
+chantype nodeOut225 = 
+	nodeOut225 :: real	
+	
+chantype nodeOut224 = 
+	nodeOut224 :: real	
+	
+chantype nodeOut223 = 
+	nodeOut223 :: real	
+	
+chantype nodeOut222 = 
+	nodeOut222 :: real	
+	
+chantype nodeOut221 = 
+	nodeOut221 :: real	
+	
+chantype layerRes22 = 
+	layerRes22 :: real	
+	
+chantype nodeOut215 = 
+	nodeOut215 :: real	
+	
+chantype nodeOut214 = 
+	nodeOut214 :: real	
+	
+chantype nodeOut213 = 
+	nodeOut213 :: real	
+	
+chantype nodeOut212 = 
+	nodeOut212 :: real	
+	
+chantype nodeOut211 = 
+	nodeOut211 :: real	
+	
+chantype layerRes21 = 
+	layerRes21 :: real	
+	
+chantype nodeOut1510 = 
+	nodeOut1510 :: real	
+	
+chantype nodeOut159 = 
+	nodeOut159 :: real	
+	
+chantype nodeOut158 = 
+	nodeOut158 :: real	
+	
+chantype nodeOut157 = 
+	nodeOut157 :: real	
+	
+chantype nodeOut156 = 
+	nodeOut156 :: real	
+	
+chantype nodeOut155 = 
+	nodeOut155 :: real	
+	
+chantype nodeOut154 = 
+	nodeOut154 :: real	
+	
+chantype nodeOut153 = 
+	nodeOut153 :: real	
+	
+chantype nodeOut152 = 
+	nodeOut152 :: real	
+	
+chantype nodeOut151 = 
+	nodeOut151 :: real	
+	
+chantype layerRes15 = 
+	layerRes15 :: real	
+	
+chantype nodeOut1410 = 
+	nodeOut1410 :: real	
+	
+chantype nodeOut149 = 
+	nodeOut149 :: real	
+	
+chantype nodeOut148 = 
+	nodeOut148 :: real	
+	
+chantype nodeOut147 = 
+	nodeOut147 :: real	
+	
+chantype nodeOut146 = 
+	nodeOut146 :: real	
+	
+chantype nodeOut145 = 
+	nodeOut145 :: real	
+	
+chantype nodeOut144 = 
+	nodeOut144 :: real	
+	
+chantype nodeOut143 = 
+	nodeOut143 :: real	
+	
+chantype nodeOut142 = 
+	nodeOut142 :: real	
+	
+chantype nodeOut141 = 
+	nodeOut141 :: real	
+	
+chantype layerRes14 = 
+	layerRes14 :: real	
+	
+chantype nodeOut1310 = 
+	nodeOut1310 :: real	
+	
+chantype nodeOut139 = 
+	nodeOut139 :: real	
+	
+chantype nodeOut138 = 
+	nodeOut138 :: real	
+	
+chantype nodeOut137 = 
+	nodeOut137 :: real	
+	
+chantype nodeOut136 = 
+	nodeOut136 :: real	
+	
+chantype nodeOut135 = 
+	nodeOut135 :: real	
+	
+chantype nodeOut134 = 
+	nodeOut134 :: real	
+	
+chantype nodeOut133 = 
+	nodeOut133 :: real	
+	
+chantype nodeOut132 = 
+	nodeOut132 :: real	
+	
+chantype nodeOut131 = 
+	nodeOut131 :: real	
+	
+chantype layerRes13 = 
+	layerRes13 :: real	
+	
+chantype nodeOut1210 = 
+	nodeOut1210 :: real	
+	
+chantype nodeOut129 = 
+	nodeOut129 :: real	
+	
+chantype nodeOut128 = 
+	nodeOut128 :: real	
+	
+chantype nodeOut127 = 
+	nodeOut127 :: real	
+	
+chantype nodeOut126 = 
+	nodeOut126 :: real	
+	
+chantype nodeOut125 = 
+	nodeOut125 :: real	
+	
+chantype nodeOut124 = 
+	nodeOut124 :: real	
+	
+chantype nodeOut123 = 
+	nodeOut123 :: real	
+	
+chantype nodeOut122 = 
+	nodeOut122 :: real	
+	
+chantype nodeOut121 = 
+	nodeOut121 :: real	
+	
+chantype layerRes12 = 
+	layerRes12 :: real	
+	
+chantype nodeOut1110 = 
+	nodeOut1110 :: real	
+	
+chantype nodeOut119 = 
+	nodeOut119 :: real	
+	
+chantype nodeOut118 = 
+	nodeOut118 :: real	
+	
+chantype nodeOut117 = 
+	nodeOut117 :: real	
+	
+chantype nodeOut116 = 
+	nodeOut116 :: real	
+	
+chantype nodeOut115 = 
+	nodeOut115 :: real	
+	
+chantype nodeOut114 = 
+	nodeOut114 :: real	
+	
+chantype nodeOut113 = 
+	nodeOut113 :: real	
+	
+chantype nodeOut112 = 
+	nodeOut112 :: real	
+	
+chantype nodeOut111 = 
+	nodeOut111 :: real	
+	
+chantype layerRes11 = 
+	layerRes11 :: real	
+	
+chantype layerRes010 = 
+	layerRes010 :: real	
+	
+chantype layerRes09 = 
+	layerRes09 :: real	
+	
+chantype layerRes08 = 
+	layerRes08 :: real	
+	
+chantype layerRes07 = 
+	layerRes07 :: real	
+	
+chantype layerRes06 = 
+	layerRes06 :: real	
+	
+chantype layerRes05 = 
+	layerRes05 :: real	
+	
+chantype layerRes04 = 
+	layerRes04 :: real	
+	
+chantype layerRes03 = 
+	layerRes03 :: real	
+	
+chantype layerRes02 = 
+	layerRes02 :: real	
+	
+chantype layerRes01 = 
+	layerRes01 :: real	
+	
+chantype terminate = 
+	terminate :: unit 	
+
+ 
+
+end
+
+
